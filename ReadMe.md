a3af4f2324b8054f80d01a6541b58998cdbcaba6
Rem Simiyu <remsimiyu@gmail.com>
2025-01-27 02:47:52 +0300

    Javascript Full Course

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+JavaScript is a high-level, 0bject-oriented, multi-paradigm programming language.
---
+++ fundamentals_1/values_variables.js
@@ -0,0 +1,6 @@
+let js  =  "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
