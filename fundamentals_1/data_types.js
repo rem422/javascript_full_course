cf49b0ab11f80fb4864bbb77934f095b30f562ba
Rem Simiyu <remsimiyu@gmail.com>
2025-01-27 03:14:58 +0300

    JavaScript Full Course

---
+++ fundamentals_1/data_types.js
@@ -0,0 +1,18 @@
+//Data Types
+// object and premitive data types
+
+//Premitive data types:
+
+//1. Numbers - floating point numbers, used for decimals and integers.
+//1. Strings - Sequence of characters, used for text.
+//3. Booleans - Logical type that only can be true or false, used for taking decisions.
+//4. Undefined - Value taken by variable that is not yet defined ("empty value").
+//5. Null - Also means 'empty value.
+//6. Symbol (ES2015) - Value that is unique and cannot be changed [Not usefull for now].
+//7. Big Int (ES2020) - Larger integers that the number type can not hold.
+
+// JavaScript has dynamic typing: We do not have to manually define the type of the value stored in a variable. Instead, data types are determined automatically.
+
+
+
+
--- fundamentals_1/values_variables.js
+++ fundamentals_1/values_variables.js
@@ -4,3 +4,20 @@ console.log(40 + 8 + 23 - 10);
 console.log("Jonas");
 console.log(23);
 
+let firstName  = 'Rem';
+console.log(firstName);
+
+//conventions and roules for naming variables
+//avoid usign numbers and js reserved names or starting a variable name with uppercase letter.
+
+//camel case
+let lastName = "Simiyu";
+// underscore
+let last_Name = "Simiyu";
+//dollar sign
+$age = 25;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+
