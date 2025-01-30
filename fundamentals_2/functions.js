da48fc592af54d02693c5e77e3c67673b82c7fb6
Rem Simiyu <remsimiyu@gmail.com>
2025-01-30 19:04:11 +0300

    Javascript full course

--- fundamentals_2/functions.js
+++ fundamentals_2/functions.js
@@ -1,1 +1,23 @@
 'use strict'
+
+// A function is a piece of code that we can reuse over and over again in our code.
+// functions allows us to write more maintainable code.
+
+function logger () {
+    console.log(`My name is Rem`);
+}
+// calling, running, invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const fruiteJuice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return fruiteJuice;
+}
+
+const appleJuice =  fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
